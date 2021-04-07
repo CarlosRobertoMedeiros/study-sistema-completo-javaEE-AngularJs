96cb8998e50d821654f44e58d7dbad795cbdb7e0
Carlos Roberto <carlosmedeiroslima@gmail.com>
2021-04-06 22:51:33 -0300

    Atualização do Swagger

--- backend/src/main/java/br/com/roberto/SiscompApplication.java
+++ backend/src/main/java/br/com/roberto/SiscompApplication.java
@@ -6,20 +6,24 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.logging.Logger;
 
+//Continuar a partir dos 7 minutos do video
+//https://synaptiklabs.com/posts/adding-swagger-to-a-javaee-project-javaee-series-2-part-2/
 @ApplicationPath("/api")
 public class SiscompApplication extends Application {
 
-    private static final String BASE_PATH = "siscomp-api";
-    private static final Logger LOGGER = Logger.getLogger(SiscompApplication.class.getName() );
+    //private static final String BASE_PATH = "siscomp-api";
+    //private static final Logger LOGGER = Logger.getLogger(SiscompApplication.class.getName() );
 
     public SiscompApplication() {
         BeanConfig conf = new BeanConfig();
         conf.setTitle("SisComp - Api");
         conf.setDescription("SisComp - Sistema Completo");
         conf.setVersion("1.0.0");
-        conf.setBasePath(BASE_PATH);
-        conf.setResourcePackage("br.com.roberto");
+        conf.setBasePath("/swagger-demo/api");
+        conf.setResourcePackage("br.com.roberto.v1.rest");
+
         conf.setScan(true);
         conf.setPrettyPrint(true);
+
     }
 }
